6f9c545571d6d9f56bdc07f0f5500b6e204c785d
njz <ningjz_paicheya@163.com>
2017-02-16 15:42:44 +0800

    部分功能提交

--- src/my-jquery.js
+++ src/my-jquery.js
@@ -1,9 +1,29 @@
+var div = document.createElement("DIV");
+function buildHTML(selector) {
+	selector = selector.trim();
+	//如果是构建字符串，需要判断是否是<>格式，如果不是表示是选择器
+	if(selector.startsWith("<")){
+		div.innerHTML = selector;
+		return $(div.children);
+	} else {
+		return $();
+	}
+}
+
+function access(myjq, setter, getter, key, value) {
+	//是否是setter方法,如果是setter方法,value不能是undefined
+	return value === undefined ? getter(myjq[0], key) : myjq.each(function (item) {
+		return setter(key, value)
+	})
+}
+
 var $ = function(selector){
 	return new $.fn.init(selector);
 }
 
 $.fn = $.prototype = {
 	init : function(selector){
+
 		//如果没有选择器，直接返回
 		if(!selector){
 			return this;
@@ -13,10 +33,8 @@ $.fn = $.prototype = {
 			//如果是字符串，表示可能是选择器，或者是构建字符串
 			selector = selector.trim();
 			//如果是构建字符串，需要判断是否是<>格式，如果不是表示是选择器
-			if(selector.trim().startsWith("<")){
-				var div = document.createElement("DIV")
-				div.innerHTML = selector;
-				return $(div.children);
+			if(selector.startsWith("<")){
+				return $(buildHTML(div.children));
 			} else {
 				//如果不是，说明是选择器，直接选择
 				var doms = document.querySelectorAll(selector);
@@ -41,20 +59,24 @@ $.fn = $.prototype = {
 			this[0] = selector;
 			return this;
 		}
-
-
 	},
 
 	//使init变为类数组对象
 	length : 0,
 
+	//模仿each
+	each : function (fn) {
+		for(var i =0; i < this.length; i++){
+			typeof fn == 'function' && fn.call(this[i], i, this[i]);
+		}
+		return this;
+	},
+
 	//在某个节点下面找div
 	find: function(selector){
-		//如果不是，说明是选择器，直接选择
-		var doms = document.querySelectorAll(selector);
-
-		//递归调用$，从新封装一次
-		return $(doms);
+		return access(this,null,function (dom, selector) {
+			return $(dom.querySelectorAll(selector));
+		},selector)
 	},
 
 	append : function(child){
@@ -92,19 +114,19 @@ $.fn = $.prototype = {
 	},
 
 
-attr(name|pro|key,val|fn)
-removeAttr(name)
-
-addClass(class|fn)
-removeClass([class|fn])
-toggleClass(class|fn[,sw])
-
-on(eve,[sel],[data],fn)1.7+
-off(eve,[sel],[fn])
-
-html([val|fn])
-text([val|fn])
-val([val|fn|arr])
+// attr(name|pro|key,val|fn)
+// removeAttr(name)
+//
+// addClass(class|fn)
+// removeClass([class|fn])
+// toggleClass(class|fn[,sw])
+//
+// on(eve,[sel],[data],fn)1.7+
+// off(eve,[sel],[fn])
+//
+// html([val|fn])
+// text([val|fn])
+// val([val|fn|arr])
 }
 
 
